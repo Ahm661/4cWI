5593f0ba9c28b3ed15194b81fec184067ccd3f7c
ahmet <ahmet.akbulut@student.htldornbirn.at>
2024-01-11 14:37:53 +0100

    State Theorie

--- demo/src/components/PeopleContainer.js
+++ demo/src/components/PeopleContainer.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
 export default function PeopleContainer() {
-    const [people, setPeople] = useState([])
+    const [people, setPeople] = useState([]);
+    const [color, setColor] = useState("green");
 
     useEffect(() => {
         fetch("https://659fe7575023b02bfe8ac454.mockapi.io/person").then(
@@ -12,9 +13,14 @@ export default function PeopleContainer() {
         );
     }, []);
 
+    useEffect(() => {
+        alert("change")
+    }, [color])
+
     return (
         <div >
             <h1>People</h1>
+            <button onClick={() => (setColor("Orange"))}>Change Color</button>
             <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 '>
                 {people.map(person => {
                     return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle} />
