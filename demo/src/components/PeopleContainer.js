c02beeb16f232445ceb0335a6965a9f981b80788
ahmet <ahmet.akbulut@student.htldornbirn.at>
2024-01-11 14:47:43 +0100

    Filtern Dran

--- demo/src/components/PeopleContainer.js
+++ demo/src/components/PeopleContainer.js
@@ -3,7 +3,7 @@ import Card from './Card'
 
 export default function PeopleContainer() {
     const [people, setPeople] = useState([]);
-    const [color, setColor] = useState("green");
+
 
     useEffect(() => {
         fetch("https://659fe7575023b02bfe8ac454.mockapi.io/person").then(
@@ -13,14 +13,16 @@ export default function PeopleContainer() {
         );
     }, []);
 
-    useEffect(() => {
-        alert("change")
-    }, [color])
+
 
     return (
         <div >
             <h1>People</h1>
-            <button onClick={() => (setColor("Orange"))}>Change Color</button>
+            <div className='border'>
+                <input className="border p-4" type="text" placeholder="search" onChange={(el) => {
+                    console.log(el.target.value);
+                }} />
+            </div>
             <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 '>
                 {people.map(person => {
                     return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle} />
