af50aa8b303f96b2a6c70c8e6c2952098683b431
ahmet <ahmet.akbulut@student.htldornbirn.at>
2024-01-11 14:26:37 +0100

    Projekt Cards & Daten Fertig

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import Button from './components/Button';
+import Card from './components/Card';
+import PeopleContainer from './components/PeopleContainer';
 
 function App() {
   return (
-    <div className='bg-green-400'>
-        <h1>Hello</h1>
+    <div>
+      <PeopleContainer />
     </div>
   );
 }
