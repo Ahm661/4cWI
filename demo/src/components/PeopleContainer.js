43bb7145283820c4ebf5ec02fe591e6af36520db
ahmet <ahmet.akbulut@student.htldornbirn.at>
2024-01-18 13:45:12 +0100

    Demo-4 Finished

--- demo/src/components/PeopleContainer.js
+++ demo/src/components/PeopleContainer.js
@@ -3,28 +3,38 @@ import Card from './Card'
 
 export default function PeopleContainer() {
     const [people, setPeople] = useState([]);
+    const [filteredPeople, setFilteredPeople] = useState([]);
 
 
     useEffect(() => {
         fetch("https://659fe7575023b02bfe8ac454.mockapi.io/person").then(
             (res) => res.json().then((data) => {
                 setPeople(data);
+                setFilteredPeople(data);
             })
         );
     }, []);
 
+    const filterPeople = (Filter) => {
+        let filtered = people.filter(person => person.name.toLowerCase().includes(Filter.toLowerCase));
+        setFilteredPeople(filtered);
+    }
+
+
 
 
     return (
-        <div >
-            <h1>People</h1>
-            <div className='border'>
+        <div className=''>
+
+            <div className='fixed border mb-4 bg-white w-full h-28 p-8'>
+
                 <input className="border p-4" type="text" placeholder="search" onChange={(el) => {
                     console.log(el.target.value);
+                    filterPeople(el.target.value);
                 }} />
             </div>
-            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 '>
-                {people.map(person => {
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32'>
+                {filteredPeople.map(person => {
                     return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle} />
                 })}
 
